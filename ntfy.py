bc818071593ccb81bab379c7467c566c4c291232
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2014-10-22 15:39:52 +0900

    Add default icon url

--- ntfy.py
+++ ntfy.py
@@ -22,7 +22,7 @@ def main(args):
     if args.to:
         message = '@' + args.to + ' ' + message
 
-    payload = {'text': message, 'username': args.name}
+    payload = {'text': message, 'username': args.name, 'icon_url': ''}
     r = requests.post(args.url, data=json.dumps(payload))
 
 if __name__ == "__main__":
@@ -32,5 +32,6 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--num', required=False, default=100, help='Number of lines of stdin included in e-mail')
     parser.add_argument('-f', '--name', required=False, default='ntfy bot', help='Name shown on slack')
     parser.add_argument('-e', '--echo', action='store_true', help='Turn on echo back')
+    parser.add_argument('-i', '--icon', required=False, default='http://thorikawa.github.io/ntfy/icon_128.png', help='Icon URL')
     args = parser.parse_args()
     main(args)
